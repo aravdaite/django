6e0b6480e1e0e820ee6655542e430b10887befcd
aravdaite <aravdaite@gmail.com>
2020-10-12 22:12:36 +0200

    added models and tests, fixed typos

---
+++ app/core/tests/test_models.py
@@ -0,0 +1,45 @@
+
+# set up some tests
+# run: docker-compose run app sh -c "python manage.py test && flake8"
+# to create core folder, run: docker-compose run app sh -c "python manage.py startapp core"
+from django.test import TestCase
+from django.contrib.auth import get_user_model
+
+
+class ModelTests(TestCase):
+
+    def test_create_user_with_email_successful(self):
+        """Test creating new user with an email is successful"""
+        email = 'abc@gmail.com'
+        password = '123456'
+        user = get_user_model().objects.create_user(
+            email=email,
+            password=password
+        )
+
+        self.assertEqual(user.email, email)
+        self.assertTrue(user.check_password(password))
+
+    def test_new_user_email_normalized(self):
+        """Test the email for a new user is normalized"""
+        email = 'test@GMAIL.com'
+        user = get_user_model().objects.create_user(email, 'test123')
+
+        self.assertEqual(user.email, email.lower())
+
+    def test_new_user_invalid_email(self):
+        """Test creating user with no email raises error"""
+        with self.assertRaises(ValueError):
+            get_user_model().objects.create_user(None, 'test123')
+
+    def test_create_new_superuser(self):
+        """Test creating a new superuser"""
+        user = get_user_model().objects.create_superuser(
+            'test@gmail.com',
+            '123456'
+        )
+
+        self.assertTrue(user.is_superuser)
+        self.assertTrue(user.is_staff)
+
+# run: docker-compose run app sh -c "python manage.py test"
